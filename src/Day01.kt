f955c7ecc815adb98798fe24172c47c318bab61c
Tom Siewert <tom.siewert@hetzner.com>
2021-12-01 15:27:39 +0100

    Add Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val input = input.map { it.toInt() }
+        var count =  0
+        for (i in 1 until input.size) {
+            if (input[i-1] < input[i]) count++
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val input = input.map { it.toInt() }
+        var count = 0
+        var lastSum = 0
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        input.windowed(3).forEach { window ->
+            val sum = window.sum()
+            if (sum > lastSum && lastSum != 0) {
+                count++
+            }
+            lastSum = sum
+        }
+        return count
+    }
 
     val input = readInput("Day01")
     println(part1(input))
